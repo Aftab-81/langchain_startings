009e4ec9c5950efaa0b08c9dc7a11b36a3d0c4db
Aftabalam Makandar <makandaraftabalam@gmail.com>
2026-08-08 12:52:05 +0530

    3 types of Messages implemented

--- Langchain_PROMPTS/chatbot.py
+++ Langchain_PROMPTS/chatbot.py
@@ -1,4 +1,5 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,14 +11,16 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm = llm)
 
-chat_history = []
+chat_history = [
+    SystemMessage(content = "You are a help assistant")
+]
 
 while True:
     user_input = input("User: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(user_input))
     if user_input == "exit":
         break
-    chat_history.append(model.invoke(chat_history).content)
+    chat_history.append(AIMessage(model.invoke(chat_history).content))
     print("AI: ", model.invoke(chat_history).content)
 
 print(chat_history)
