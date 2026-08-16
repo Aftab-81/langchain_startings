d0bc33b18d8faf44d190bff631bff906cc37aa82
Aftabalam Makandar <makandaraftabalam@gmail.com>
2026-08-16 15:52:36 +0530

    Undrestand the Runnable Primitive - Runnable LCEL

---
+++ Runnables/Runnable_Primitives/runnable_lcrl.py
@@ -0,0 +1,38 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableSequence
+from langchain_core.runnables import RunnableParallel
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnableLambda
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id = "openai/gpt-oss-120b",
+    task = "text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+template1 = PromptTemplate(
+    template = "Write a joke on {topic_of_joke}",
+    input_variables = ["topic_of_joke"]
+)
+
+template2 = PromptTemplate(
+    template = "Give a 4-6 lines of Summary to the following: {text}",
+    input_variables = ["text"]
+)
+
+parser = StrOutputParser()
+
+chain1 = template1 | model | parser 
+chain2 = template2 | model | parser
+
+final_chain = chain1 | chain2
+
+result = final_chain.invoke({"topic_of_joke": "Hawaii"})
+
+print(result)
